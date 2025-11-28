645c5d521ddb1d1a8c4ffca5a6c7808bd7de6786
Anton Engelhardt <antoncengelhardt@icloud.com>
2025-11-28 17:11:55 +0100

    fix: update cookie attributes to include HttpOnly
    Signed-off-by: Anton Engelhardt <antoncengelhardt@icloud.com>

--- src/session.rs
+++ src/session.rs
@@ -102,18 +102,18 @@ impl Session {
         // Build the cookie values
         for (i, cookie_part) in cookie_parts.enumerate() {
             let cookie_value = format!(
-                "{cookie_name}-{i}={cookie_part}; Path=/; Secure;Max-Age={cookie_duration}; SameSite=Lax",
+                "{cookie_name}-{i}={cookie_part}; Path=/;Secure;HttpOnly;Max-Age={cookie_duration}; SameSite=Lax",
             );
             cookie_values.push(cookie_value);
         }
 
         let num_parts = cookie_values.len();
-        let num_parts_cookie_value = format!("{cookie_name}-parts={num_parts}; Path=/; Secure; Max-Age={cookie_duration}; SameSite=Lax");
+        let num_parts_cookie_value = format!("{cookie_name}-parts={num_parts}; Path=/;Secure;HttpOnly;Max-Age={cookie_duration}; SameSite=Lax");
         cookie_values.push(num_parts_cookie_value);
 
         // Build nonce cookie value
         let nonce_cookie_value = format!(
-            "{cookie_name}-nonce={encoded_nonce}; Path=/; Secure;Max-Age={cookie_duration};SameSite=Lax",
+            "{cookie_name}-nonce={encoded_nonce}; Path=/;Secure;HttpOnly;Max-Age={cookie_duration};SameSite=Lax",
         );
         cookie_values.push(nonce_cookie_value);
 
